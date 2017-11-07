ae3f9de4f26f08a771a7f669680dbd60d03cdbe4
Jacob Sherman <jacob@jbsherman.com>
2017-11-07 12:50:12 -0500

    Initial commit: finish learnredux course code

---
+++ client/store.js
@@ -0,0 +1,29 @@
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore} from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for the default data
+const defaultState = {
+  posts,
+  comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+// hot reload reducers function
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    const nextRootReducer = require('./reducers/index').default;   // cant use es6 import inside of function
+      store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
---
+++ readme.md
@@ -0,0 +1,18 @@
+
+# Reduxstagram
+
+This is a basic Instragram clone using React and Redux from the 'Learn Redux' course by Wes Boz. 
+You can find the free course here <https://learnredux.com/>.  The code is using some outdated modules such as React Router 2.
+I hope to update the code and extend this app to take in a live api feed and update some of the React syntax
+
+## Running
+
+First `npm install` to grab all the necessary dependencies. 
+
+You may get errors because the Sentry.io config file is not part of this repo.   
+
+Then run `npm start` and open <localhost:7770> in your browser.
+
+## Production Build
+
+Run `npm build` to create a distro folder and a bundle.js file.
